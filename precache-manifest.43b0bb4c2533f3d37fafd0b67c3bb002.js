c06a8c62f4dd4bc10fafeedabd8b53eb0dd4baa0
Coditas <sachin.pardeshi@coditas.com>
2020-08-09 16:07:03 +0530

    Updates

---
+++ precache-manifest.43b0bb4c2533f3d37fafd0b67c3bb002.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9239ca8123deac2e6f528a5bae7f74f0",
+    "url": "/trivia-game/index.html"
+  },
+  {
+    "revision": "02e0a9bd9dda9f1bf5a2",
+    "url": "/trivia-game/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "f22ec5f1a877600f0fc4",
+    "url": "/trivia-game/static/css/main.e64f276c.chunk.css"
+  },
+  {
+    "revision": "02e0a9bd9dda9f1bf5a2",
+    "url": "/trivia-game/static/js/2.f729be89.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/trivia-game/static/js/2.f729be89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f22ec5f1a877600f0fc4",
+    "url": "/trivia-game/static/js/main.d93a4d50.chunk.js"
+  },
+  {
+    "revision": "e1ca65767f18f993100f",
+    "url": "/trivia-game/static/js/runtime-main.f3da5c0f.js"
+  },
+  {
+    "revision": "03953fac26e8f11c2643d99a02b09a9f",
+    "url": "/trivia-game/static/media/casino-bg.03953fac.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/%7Btrivia-game%7D/precache-manifest.fd32ce1ceebe3a2f5c4d9b77ed9a54e9.js"
+  "/trivia-game/precache-manifest.43b0bb4c2533f3d37fafd0b67c3bb002.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/%7Btrivia-game%7D/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/trivia-game/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
